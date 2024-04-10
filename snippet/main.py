4a35a0b307ec4a9636d061d9c10e4efcce7e6fa9
Mr-Ahmadi <mr-ahmadi2004@outlook.com>
2024-04-10 14:36:57 +0330

    clean code :)

--- snippet/main.py
+++ snippet/main.py
@@ -5,14 +5,14 @@ from grammar.pcfg import PCFG
 grammar_file = sys.argv[1]
 train_file = sys.argv[2]
 
-pcfg = PCFG(grammar_file, "./test.pcfg")
+pcfg = PCFG(grammar_file)
 
 unary_rules = pcfg.grammar.unary_rules
 binary_rules = pcfg.grammar.binary_rules
 
-# q = pcfg.estimate(train_file, 5)
-
-print(pcfg.gen_sentence("S"))
+q = pcfg.estimate(train_file, 5)
+print(q)
+# print(pcfg.gen_sentence("S"))
 
 # for A, B, C in binary_rules:
 #     print(A + " -> " + B + " " + C + " " + str(q.get((A, B, C))))
@@ -21,4 +21,4 @@ print(pcfg.gen_sentence("S"))
 #     print(A + " -> " + w + " " + str(q.get((A, w))))
 
 # print(pcfg.grammar.parsable("s s"))
-print(pcfg.sentence_prob("s s s s s s d d s s s s d d s"))
+# print(pcfg.sentence_prob("s s s s s s d d s s s s d d s"))
